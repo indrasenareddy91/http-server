2a43d58f56a621526bb597ff70934d317864a3fd
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-07-23 14:56:03 +0530

    response

--- app/main.js
+++ app/main.js
@@ -4,6 +4,9 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
+  socket.on("connect", () => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  });
   socket.on("close", () => {
     socket.end();
   });
