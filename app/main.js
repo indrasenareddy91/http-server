db6b4efa67a76598bf553089c4e9cb2f9ba5998f
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-07-23 15:16:22 +0530

    parsed the url

--- app/main.js
+++ app/main.js
@@ -4,9 +4,17 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-
-  socket.on("close", () => {
+  socket.on("data", (data) => {
+    console.log(data);
+    const request = data.toString();
+    console.log(request);
+    // so to get the path we just need to split the string with space delimeter and it will return an array
+    const path = request.split(" ")[1];
+    const status = path === "/" ? "200 OK" : "400 Not Found";
+    socket.write(`HTTP/1.1 ${status}\r\n\r\n`);
+  });
+  socket.on("close", (data) => {
+    console.log(data);
     socket.end();
     socket.close();
   });
