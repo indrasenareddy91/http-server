ed3970ee96ed0ed742cc9faa80874930610a373b
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-07-23 15:17:33 +0530

    hello

--- app/main.js
+++ app/main.js
@@ -5,13 +5,14 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    console.log(data);
+    console.log(data); //buffer object with ascii
     const request = data.toString();
     console.log(request);
     // so to get the path we just need to split the string with space delimeter and it will return an array
     const path = request.split(" ")[1];
-    const status = path === "/" ? "200 OK" : "400 Not Found";
+    const status = path === "/" ? "200 OK" : "404 Not Found";
     socket.write(`HTTP/1.1 ${status}\r\n\r\n`);
+    socket.end();
   });
   socket.on("close", (data) => {
     console.log(data);
